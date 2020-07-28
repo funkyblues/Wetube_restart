f8c6a4eef411ca742e1e55da9682d0df3a9215a7
funkyblues <dnr8874@naver.com>
2020-07-29 00:45:24 +0900

    메이플 접고... 뭐 하고 싶은지 이것 저것 해보자

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-import morgan from 'morgan';
-import express from 'express';
-import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import { userRouter } from './router';
+import morgan from "morgan";
+import express from "express";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
-
-const handleHome = (req, res) => res.send('Hello from 연구실');
-
-const handleProfile = (req, res) => res.send('You are on my profile.');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', handleHome);
-
-app.get('/profile', handleProfile);
-
-app.use('/user', userRouter);
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
